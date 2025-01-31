032119bed6b4a39d8de5b5e0737bf826b035ef91
Biprodas Roy <biprodas.cse@gmail.com>
2025-01-31 10:15:45 +0600

    added chadcnui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
   return (
     <div className="">
-      School ERP
+      School ERP <Button>Click me</Button>
     </div>
   );
 }
